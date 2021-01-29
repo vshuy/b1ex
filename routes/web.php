14bd1297bec62d586424f8d3fe42114a86d6534d
m134m4a1 <m134m4a1@gmail.com>
2021-01-29 08:23:42 +0700

    update list api

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
  */
-//Route::get('/',[readsheet::class,'HandlerSheet']);
+
 
 Route::get('/', function () {
     return view('uploadfile');
@@ -31,3 +31,17 @@ Route::get('/getlistde', [DethiController::class, 'getListDe']);
 Route::get('/getrandomidde', [PartController::class, 'getRandomIdDe']);
 Route::get('/getoneexam', [PartController::class, 'getRandomAnExam']);
 
+Route::get('/getpart1', [PartController::class, 'getPart1']);
+Route::get('/getpart2', [PartController::class, 'getPart2']);
+Route::get('/getpart3.1', [PartController::class, 'getPart3dot1']);
+Route::get('/getpart3.2', [PartController::class, 'getPart3dot2']);
+Route::get('/getpart4', [PartController::class, 'getPart4']);
+Route::get('/getpart5', [PartController::class, 'getPart5']);
+Route::get('/getpart6', [PartController::class, 'getPart6']);
+Route::get('/getpart7', [PartController::class, 'getPart7']);
+Route::get('/getpart8', [PartController::class, 'getPart8']);
+Route::get('/getpart9', [PartController::class, 'getPart9']);
+Route::get('/getpart10', [PartController::class, 'getPart10']);
+Route::get('/getpart11', [PartController::class, 'getPart11']);
+Route::get('/getpart12', [PartController::class, 'getPart12']);
+Route::get('/getpart13', [PartController::class, 'getPart13']);
