bb5666301ff15de5c1717bbeb2b6d3544a5e435e
m134m4a1 <m134m4a1@gmail.com>
2021-09-25 22:25:38 +0700

    update api template

--- app/Http/Controllers/PartController.php
+++ app/Http/Controllers/PartController.php
@@ -38,20 +38,20 @@ class PartController extends Controller
     public function detailAnExam($idde)
     {
         $aExam = collect([
-            $this->getPartInforByExamIdAndPartName($idde, '1'),
-            $this->getPartInforByExamIdAndPartName($idde, '2'),
-            $this->getPartInforByExamIdAndPartName($idde, '3.1'),
-            $this->getPartInforByExamIdAndPartName($idde, '3.2'),
-            $this->getPartInforByExamIdAndPartName($idde, '4'),
-            $this->getPartInforByExamIdAndPartName($idde, '5'),
-            $this->getPartInforByExamIdAndPartName($idde, '6'),
-            $this->getPartInforByExamIdAndPartName($idde, '7'),
-            $this->getPartInforByExamIdAndPartName($idde, '8'),
-            $this->getPartInforByExamIdAndPartName($idde, '9'),
-            $this->getPartInforByExamIdAndPartName($idde, '10'),
-            $this->getPartInforByExamIdAndPartName($idde, '11'),
-            $this->getPartInforByExamIdAndPartName($idde, '12'),
-            $this->getPartInforByExamIdAndPartName($idde, '13'),
+            $this->getPartInforByExamIdAndPartNameNoImg($idde, '1'),
+            $this->getPartInforByExamIdAndPartNameNoImg($idde, '2'),
+            $this->getPartInforByExamIdAndPartNameNoImg($idde, '3.1'),
+            $this->getPartInforByExamIdAndPartNameNoImg($idde, '3.2'),
+            $this->getPartInforByExamIdAndPartNameNoImg($idde, '4'),
+            $this->getPartInforByExamIdAndPartNameNoImg($idde, '5'),
+            $this->getPartInforByExamIdAndPartNameNoImg($idde, '6'),
+            $this->getPartInforByExamIdAndPartNameNoImg($idde, '7'),
+            $this->getPartInforByExamIdAndPartNameNoImg($idde, '8'),
+            $this->getPartInforByExamIdAndPartNameNoImg($idde, '9'),
+            $this->getPartInforByExamIdAndPartNameNoImg($idde, '10'),
+            $this->getPartInforByExamIdAndPartNameNoImg($idde, '11'),
+            $this->getPartInforByExamIdAndPartNameNoImg($idde, '12'),
+            $this->getPartInforByExamIdAndPartNameNoImg($idde, '13'),
         ]);
         //  return $aExam[0]['part'][0]->id;
         //  return $aExam[0]['questions'][0]->noidung_cauhoi;
@@ -76,21 +76,38 @@ class PartController extends Controller
         }
         $listPartDocumentArray = $this->getPartDocumentByQuestionId($idQuestionArray[0]);
         $listAnswerOfPart = DB::table('dapans')
-            ->whereIn('cauhoi_id', $idQuestionArray)
-            ->get();
-        $idAnswerArray = [];
-        foreach ($listAnswerOfPart as $aAnswer) {
-            $idAnswerArray[] = $aAnswer->id;
-        }
-        $listDocumentOfPart = DB::table('tailieu_dapans')
-            ->select('tailieu_dapans.dapan_id', 'tailieu_dapans.url')
-            ->whereIn('dapan_id', $idAnswerArray)
+            ->join('tailieu_dapans', 'tailieu_dapans.dapan_id', '=', 'dapans.id')
+            ->select('dapans.id', 'dapans.cauhoi_id', 'dapans.noidung_dapan', 'dapans.dapan', 'tailieu_dapans.url')
+            ->whereIn('dapans.cauhoi_id', $idQuestionArray)
             ->get();
         $result = collect([
             "listPartDocumentArray" => $listPartDocumentArray,
             "questions" => $listQuestionOfPart,
             "answers" => $listAnswerOfPart,
-            "document" => $listDocumentOfPart,
+        ]);
+        return $result;
+    }
+    public function getPartInforByExamIdAndPartNameNoImg($idde, $partName)
+    {
+        $listQuestionOfPart  = DB::table('cauhois')
+            ->join('dethi_cauhois', 'dethi_cauhois.cauhoi_id', '=', 'cauhois.id')
+            ->join('phans', 'phans.id', '=', 'cauhois.phan_id')
+            ->select('cauhois.id', 'cauhois.phan_id', 'cauhois.noidung_cauhoi')
+            ->where('dethi_cauhois.dethi_id', $idde)
+            ->where('phans.name', $partName)
+            ->get();
+        $idQuestionArray = [];
+        foreach ($listQuestionOfPart as $aQuestion) {
+            $idQuestionArray[] = $aQuestion->id;
+        }
+        $listPartDocumentArray = $this->getPartDocumentByQuestionId($idQuestionArray[0]);
+        $listAnswerOfPart = DB::table('dapans')
+            ->whereIn('cauhoi_id', $idQuestionArray)
+            ->get();
+        $result = collect([
+            "listPartDocumentArray" => $listPartDocumentArray,
+            "questions" => $listQuestionOfPart,
+            "answers" => $listAnswerOfPart,
         ]);
         return $result;
     }
@@ -99,19 +116,19 @@ class PartController extends Controller
     {
         $oneExam = collect([
             "part1" => $this->getPartInforByExamIdAndPartName($idde, '1'),
-            "part2" => $this->getPartInforByExamIdAndPartName($idde, '2'),
-            "part3dot1" => $this->getPartInforByExamIdAndPartName($idde, '3.1'),
-            "part3dot2" => $this->getPartInforByExamIdAndPartName($idde, '3.2'),
-            "part4" => $this->getPartInforByExamIdAndPartName($idde, '4'),
-            "part5" => $this->getPartInforByExamIdAndPartName($idde, '5'),
-            "part6" => $this->getPartInforByExamIdAndPartName($idde, '6'),
-            "part7" => $this->getPartInforByExamIdAndPartName($idde, '7'),
-            "part8" => $this->getPartInforByExamIdAndPartName($idde, '8'),
+            "part2" => $this->getPartInforByExamIdAndPartNameNoImg($idde, '2'),
+            "part3dot1" => $this->getPartInforByExamIdAndPartNameNoImg($idde, '3.1'),
+            "part3dot2" => $this->getPartInforByExamIdAndPartNameNoImg($idde, '3.2'),
+            "part4" => $this->getPartInforByExamIdAndPartNameNoImg($idde, '4'),
+            "part5" => $this->getPartInforByExamIdAndPartNameNoImg($idde, '5'),
+            "part6" => $this->getPartInforByExamIdAndPartNameNoImg($idde, '6'),
+            "part7" => $this->getPartInforByExamIdAndPartNameNoImg($idde, '7'),
+            "part8" => $this->getPartInforByExamIdAndPartNameNoImg($idde, '8'),
             "part9" => $this->getPartInforByExamIdAndPartName($idde, '9'),
-            "part10" => $this->getPartInforByExamIdAndPartName($idde, '10'),
-            "part11" => $this->getPartInforByExamIdAndPartName($idde, '11'),
-            "part12" => $this->getPartInforByExamIdAndPartName($idde, '12'),
-            "part13" => $this->getPartInforByExamIdAndPartName($idde, '13'),
+            "part10" => $this->getPartInforByExamIdAndPartNameNoImg($idde, '10'),
+            "part11" => $this->getPartInforByExamIdAndPartNameNoImg($idde, '11'),
+            "part12" => $this->getPartInforByExamIdAndPartNameNoImg($idde, '12'),
+            "part13" => $this->getPartInforByExamIdAndPartNameNoImg($idde, '13'),
         ]);
         return $oneExam;
     }
@@ -121,35 +138,35 @@ class PartController extends Controller
     }
     public function getPart2ById($idde)
     {
-        return $this->getPartInforByExamIdAndPartName($idde, '2');
+        return $this->getPartInforByExamIdAndPartNameNoImg($idde, '2');
     }
     public function getPart3dot1ById($idde)
     {
-        return $this->getPartInforByExamIdAndPartName($idde, '3.1');
+        return $this->getPartInforByExamIdAndPartNameNoImg($idde, '3.1');
     }
     public function getPart3dot2ById($idde)
     {
-        return $this->getPartInforByExamIdAndPartName($idde, '3.2');
+        return $this->getPartInforByExamIdAndPartNameNoImg($idde, '3.2');
     }
     public function getPart4ById($idde)
     {
-        return $this->getPartInforByExamIdAndPartName($idde, '4');
+        return $this->getPartInforByExamIdAndPartNameNoImg($idde, '4');
     }
     public function getPart5ById($idde)
     {
-        return $this->getPartInforByExamIdAndPartName($idde, '5');
+        return $this->getPartInforByExamIdAndPartNameNoImg($idde, '5');
     }
     public function getPart6ById($idde)
     {
-        return $this->getPartInforByExamIdAndPartName($idde, '6');
+        return $this->getPartInforByExamIdAndPartNameNoImg($idde, '6');
     }
     public function getPart7ById($idde)
     {
-        return $this->getPartInforByExamIdAndPartName($idde, '7');
+        return $this->getPartInforByExamIdAndPartNameNoImg($idde, '7');
     }
     public function getPart8ById($idde)
     {
-        return $this->getPartInforByExamIdAndPartName($idde, '8');
+        return $this->getPartInforByExamIdAndPartNameNoImg($idde, '8');
     }
     public function getPart9ById($idde)
     {
@@ -157,18 +174,18 @@ class PartController extends Controller
     }
     public function getPart10ById($idde)
     {
-        return $this->getPartInforByExamIdAndPartName($idde, '10');
+        return $this->getPartInforByExamIdAndPartNameNoImg($idde, '10');
     }
     public function getPart11ById($idde)
     {
-        return $this->getPartInforByExamIdAndPartName($idde, '11');
+        return $this->getPartInforByExamIdAndPartNameNoImg($idde, '11');
     }
     public function getPart12ById($idde)
     {
-        return $this->getPartInforByExamIdAndPartName($idde, '12');
+        return $this->getPartInforByExamIdAndPartNameNoImg($idde, '12');
     }
     public function getPart13ById($idde)
     {
-        return $this->getPartInforByExamIdAndPartName($idde, '13');
+        return $this->getPartInforByExamIdAndPartNameNoImg($idde, '13');
     }
 }
