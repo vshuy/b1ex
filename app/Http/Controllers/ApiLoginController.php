3fc6c09329c6f7103573d81969d435ac247b5599
m134m4a1 <m134m4a1@gmail.com>
2021-10-22 20:57:25 +0700

    update login message feature again

--- app/Http/Controllers/ApiLoginController.php
+++ app/Http/Controllers/ApiLoginController.php
@@ -25,7 +25,7 @@ class ApiLoginController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'email' => 'required|string|email|max:255',
-            'password' => 'required|string|min:6|confirmed',
+            'password' => 'required|string|min:6',
         ]);
         if ($validator->fails()) {
             return response()->json(['success' => false, 'error' => $validator->messages()]);
